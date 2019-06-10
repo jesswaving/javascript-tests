11cf643e075e2a6f1dab995d3384dca45bbc859a
jesswaving <34092876+jesswaving@users.noreply.github.com>
2019-06-10 17:40:02 +0100

    Update flatten-array.js

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -2,6 +2,18 @@ describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+    
+    function flatten(arr)
+    var expected[]
+    for (var i = 0; i < arr.length; i++) {
+      var number = arr[i]
+      if (Array.isArray(number)) {
+        flatten(number)
+        
+      }
+      
+      expected.push(number)
+      
 
     expect(arr).toEqual(expected);
   });
